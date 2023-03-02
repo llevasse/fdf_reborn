e98279f8cb2451606821042b01e11dadb91f9830
Leo <leonardlevasseur@yahoo.com>
2023-03-02 15:07:10 +0100

    init

---
+++ includes/fdf.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/10 11:58:01 by llevasse          #+#    #+#             */
+/*   Updated: 2023/03/02 15:06:52 by llevasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+# include "../libft/libft.h"
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <fcntl.h>
+# include <math.h>
+# include <mlx.h>
+
+/* 720p */
+//# define WINDOW_WIDTH 1280
+//# define WINDOW_HEIGHT 720
+
+/* 1080p */
+# define WINDOW_WIDTH 1920
+# define WINDOW_HEIGHT 1080
+
+# define PI 3.1415926535
+
+// angle iso x = 45 y = -35 z = 30
+
+#endif
