291aec333f9dfe3e74221966671effbd15e78ed7
llevasse <leonardlevasseur@yahoo.com>
2023-07-16 11:13:07 +0200

    init window after init point

--- srcs/bonus/init_bonus.c
+++ srcs/bonus/init_bonus.c
@@ -6,7 +6,7 @@
 /*   By: llevasse <llevasse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/24 22:18:58 by llevasse          #+#    #+#             */
-/*   Updated: 2023/07/02 13:06:55 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/07/16 11:09:09 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,13 +30,6 @@ void	set_highest_n_lowest_z(t_data *data)
 int	init_data(t_data *data, int fd)
 {
 	init_null(data);
-	data->mlx_ptr = mlx_init();
-	if (!data->mlx_ptr)
-		return (1);
-	data->win_ptr = mlx_new_window(data->mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
-			"fdf");
-	if (!data->win_ptr)
-		return (free(data->mlx_ptr), 1);
 	data->angle_x = 45;
 	data->angle_y = -35;
 	data->angle_z = 30;
@@ -53,6 +46,13 @@ int	init_data(t_data *data, int fd)
 	data->beg_y = WINDOW_HEIGHT / 2;
 	data->dif_x = 0;
 	data->dif_y = 0;
+	data->mlx_ptr = mlx_init();
+	if (!data->mlx_ptr)
+		return (free_data(data, fd), 1);
+	data->win_ptr = mlx_new_window(data->mlx_ptr, WINDOW_WIDTH, WINDOW_HEIGHT,
+			"fdf");
+	if (!data->win_ptr)
+		return (free_data(data, fd), 1);
 	return (0);
 }
 
@@ -74,5 +74,9 @@ void	free_data(t_data *data, int fd)
 		mlx_destroy_display(data->mlx_ptr);
 		free(data->mlx_ptr);
 	}
+	if (data->point)
+		free(data->point);
+	if (data->line)
+		free_tab(data->line);
 	close(fd);
 }
